db115e2c0342e81b848fb77d7e9b47fadba0d2a4
Hannes <harn.hannes@gmail.com>
2024-12-18 20:13:19 +0100

    Formatted with spaces instead of tabs

--- nvim/.config/nvim/lua/plugins/utility/indent_line.lua
+++ nvim/.config/nvim/lua/plugins/utility/indent_line.lua
@@ -1,23 +1,23 @@
 return {
-	{ -- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help ibl`
-		main = "ibl",
-		opts = {
-			-- HACK: Custom indentation characters
-			indent = {
-				-- Character, or list of characters, that get used to display the indentation guide
-				char = "╎",
-			},
+    { -- Add indentation guides even on blank lines
+        "lukas-reineke/indent-blankline.nvim",
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help ibl`
+        main = "ibl",
+        opts = {
+            -- HACK: Custom indentation characters
+            indent = {
+                -- Character, or list of characters, that get used to display the indentation guide
+                char = "╎",
+            },
 
-			scope = {
-				-- Shows an underline on the first line of the scope
-				show_start = false,
+            scope = {
+                -- Shows an underline on the first line of the scope
+                show_start = false,
 
-				-- Shows an underline on the last line of the scope
-				show_end = false,
-			},
-		},
-	},
+                -- Shows an underline on the last line of the scope
+                show_end = false,
+            },
+        },
+    },
 }
--- nvim/.config/nvim/lua/plugins/utility/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/utility/nvim-ufo.lua
@@ -11,73 +11,73 @@
 -- HACK: During preview, press ENTER to jump directly to the line! So nice!
 
 return {
-	"kevinhwang91/nvim-ufo",
-	dependencies = { "kevinhwang91/promise-async" },
-	opts = {
-		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    opts = {
+        filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
 
-		open_fold_hl_timeout = 150,
+        open_fold_hl_timeout = 150,
 
-		preview = {
-			win_config = {
-				winhighlight = "Normal:Folded",
-				winblend = 0,
-			},
-			mappings = {
-				scrollU = "<C-u>",
-				scrollD = "<C-d>",
-				jumpTop = "gg",
-				jumpBot = "G",
-			},
-		},
+        preview = {
+            win_config = {
+                winhighlight = "Normal:Folded",
+                winblend = 0,
+            },
+            mappings = {
+                scrollU = "<C-u>",
+                scrollD = "<C-d>",
+                jumpTop = "gg",
+                jumpBot = "G",
+            },
+        },
 
-		fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-			local newVirtText = {}
-			local suffix = (" ... %d lines folded 󱞱"):format(endLnum - lnum)
-			local sufWidth = vim.fn.strdisplaywidth(suffix)
-			local targetWidth = width - sufWidth
-			local curWidth = 0
-			for _, chunk in ipairs(virtText) do
-				local chunkText = chunk[1]
-				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-				if targetWidth > curWidth + chunkWidth then
-					table.insert(newVirtText, chunk)
-				else
-					chunkText = truncate(chunkText, targetWidth - curWidth)
-					local hlGroup = chunk[2]
-					table.insert(newVirtText, { chunkText, hlGroup })
-					chunkWidth = vim.fn.strdisplaywidth(chunkText)
-					-- str width returned from truncate() may less than 2nd argument, need padding
-					if curWidth + chunkWidth < targetWidth then
-						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-					end
-					break
-				end
-				curWidth = curWidth + chunkWidth
-			end
-			table.insert(newVirtText, { suffix, "MoreMsg" })
-			return newVirtText
-		end,
-	},
-	config = function(_, opts)
-		vim.api.nvim_create_autocmd("FileType", {
-			group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
-			pattern = opts.filetype_exclude,
-			callback = function()
-				require("ufo").detach()
-			end,
-		})
+        fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+            local newVirtText = {}
+            local suffix = (" ... %d lines folded 󱞱"):format(endLnum - lnum)
+            local sufWidth = vim.fn.strdisplaywidth(suffix)
+            local targetWidth = width - sufWidth
+            local curWidth = 0
+            for _, chunk in ipairs(virtText) do
+                local chunkText = chunk[1]
+                local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                if targetWidth > curWidth + chunkWidth then
+                    table.insert(newVirtText, chunk)
+                else
+                    chunkText = truncate(chunkText, targetWidth - curWidth)
+                    local hlGroup = chunk[2]
+                    table.insert(newVirtText, { chunkText, hlGroup })
+                    chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    -- str width returned from truncate() may less than 2nd argument, need padding
+                    if curWidth + chunkWidth < targetWidth then
+                        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+                    end
+                    break
+                end
+                curWidth = curWidth + chunkWidth
+            end
+            table.insert(newVirtText, { suffix, "MoreMsg" })
+            return newVirtText
+        end,
+    },
+    config = function(_, opts)
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
+            pattern = opts.filetype_exclude,
+            callback = function()
+                require("ufo").detach()
+            end,
+        })
 
-		vim.keymap.set("n", "K", function()
-			local winid = require("ufo").peekFoldedLinesUnderCursor()
-			if not winid then
-				vim.lsp.buf.hover()
-			end
+        vim.keymap.set("n", "K", function()
+            local winid = require("ufo").peekFoldedLinesUnderCursor()
+            if not winid then
+                vim.lsp.buf.hover()
+            end
 
-			-- Switch into preview automatically
-			vim.api.nvim_input("<TAB>")
-		end)
+            -- Switch into preview automatically
+            vim.api.nvim_input("<TAB>")
+        end)
 
-		require("ufo").setup(opts)
-	end,
+        require("ufo").setup(opts)
+    end,
 }
