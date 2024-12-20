012d5f22a7fde9296f705cb36dccdbfc5ecc7e7d
Hannes <harn.hannes@gmail.com>
2024-12-20 21:24:51 +0100

    Configured treesitter and treesitter plugins

--- nvim/.config/nvim/lua/plugins/treesitter/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter/nvim-treesitter.lua
@@ -5,6 +5,7 @@ return {
         main = "nvim-treesitter.configs", -- Sets main module to use for opts
         -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
         opts = {
+            -- HACK: Added rust in here :)
             ensure_installed = {
                 "bash",
                 "c",
@@ -28,13 +29,34 @@ return {
                 --  the list of additional_vim_regex_highlighting and disabled languages for indent.
                 additional_vim_regex_highlighting = { "ruby" },
             },
+            -- HACK: Added these supernice keybinds that will work in any language with treesitter support, which is a lot!
             indent = { enable = true, disable = { "ruby" } },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<CR>",
+                    scope_incremental = "false",
+                    node_incremental = "<CR>",
+                    node_decremental = "<M-CR>",
+                },
+            },
         },
+
         -- There are additional nvim-treesitter modules that you can use to interact
         -- with nvim-treesitter. You should go explore a few and see what interests you:
         --
         --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
         --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
         --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    },
+    -- HACK: Adds context to se what scope you are working in!
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require("treesitter-context").setup({
+                max_lines = 5,
+                separator = " ",
+            })
+        end,
     },
 }
