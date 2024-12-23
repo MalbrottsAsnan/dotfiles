4b67c77b082651b6e1d6f17d6a2449f128113e61
Hannes <harn.hannes@gmail.com>
2024-12-23 13:45:50 +0100

    Added configuration to undotree

--- nvim/.config/nvim/lua/plugins/utility/undotree.lua
+++ nvim/.config/nvim/lua/plugins/utility/undotree.lua
@@ -1,1 +1,24 @@
-return {}
+return {
+    {
+        -- HACK: Very nice feature
+        "mbbill/undotree",
+        lazy = false,
+        config = function()
+            vim.api.nvim_set_keymap(
+                "n",
+                "<C-\\>",
+                ":UndotreeToggle<CR>",
+                { silent = true, desc = "Toggle mbbill/undotree" }
+            )
+
+            local directory = vim.fn.stdpath("data") .. "/undodir"
+
+            if vim.fn.isdirectory(directory) ~= 1 then
+                pcall(vim.fn.mkdir, directory, "p")
+            end
+
+            vim.opt.undodir = directory
+            -- vim.opt.undofile is set elsewhere
+        end,
+    },
+}
