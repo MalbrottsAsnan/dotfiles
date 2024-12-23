205c0718380d06e168d98f8bdcbf5dca459317b4
Hannes <harn.hannes@gmail.com>
2024-12-23 01:18:00 +0100

    Configured debug icons for nvim-dap

--- nvim/.config/nvim/lua/plugins/utility/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/utility/nvim-dap.lua
@@ -111,15 +111,11 @@ return {
             icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
             controls = {
                 icons = {
-                    pause = "⏸",
-                    play = "▶",
-                    step_into = "⏎",
-                    step_over = "⏭",
-                    step_out = "⏮",
-                    step_back = "b",
-                    run_last = "▶▶",
-                    terminate = "⏹",
-                    disconnect = "⏏",
+                    pause = "",
+                    play = "",
+                    run_last = "",
+                    terminate = "",
+                    disconnect = "",
                 },
             },
         })
