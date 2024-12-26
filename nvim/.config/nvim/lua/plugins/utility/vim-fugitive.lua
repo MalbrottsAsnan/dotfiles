086c94ce1e50f7b471b54a90398c37a09319d514
Hannes <harn.hannes@gmail.com>
2024-12-26 15:32:42 +0100

    Modified vim-fugitive git status keymap

--- nvim/.config/nvim/lua/plugins/utility/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/utility/vim-fugitive.lua
@@ -5,7 +5,8 @@ return {
         vim.api.nvim_set_keymap(
             "n",
             "<Leader>G",
-            ":Git<CR><C-w>7-",
+            -- ":Git<CR>",
+            ":Git<CR>7-",
             { noremap = true, silent = true, desc = "[G]it status" }
         )
     end,
