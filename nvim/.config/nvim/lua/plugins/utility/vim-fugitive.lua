8bbc36e4b390562777a80efad962d1bbe3798edf
Hannes <harn.hannes@gmail.com>
2024-12-26 15:34:06 +0100

    Modified vim-fugitive keymaps

--- nvim/.config/nvim/lua/plugins/utility/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/utility/vim-fugitive.lua
@@ -2,12 +2,6 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
     config = function()
-        vim.api.nvim_set_keymap(
-            "n",
-            "<Leader>G",
-            -- ":Git<CR>",
-            ":Git<CR>7-",
-            { noremap = true, silent = true, desc = "[G]it status" }
-        )
+        vim.api.nvim_set_keymap("n", "<Leader>G", ":Git<CR>", { noremap = true, silent = true, desc = "[G]it status" })
     end,
 }
