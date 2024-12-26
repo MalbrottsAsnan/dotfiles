6b509cdaf40899cc67c792e03f392148d5d3c8e9
Hannes <harn.hannes@gmail.com>
2024-12-26 15:39:01 +0100

    Modified vim-fugitive keybinding

--- nvim/.config/nvim/lua/plugins/utility/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/utility/vim-fugitive.lua
@@ -2,6 +2,6 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
     config = function()
-        vim.api.nvim_set_keymap("n", "<Leader>G", ":Git<CR>", { noremap = true, silent = true, desc = "[G]it status" })
+        vim.api.nvim_set_keymap("n", "<Leader>G", ":Git<CR>", { silent = true, desc = "[G]it status" })
     end,
 }
