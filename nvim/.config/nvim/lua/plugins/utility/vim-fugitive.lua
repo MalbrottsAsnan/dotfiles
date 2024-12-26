2e2c0ec019f58b52c6a6d9f36c963f8753cab7ea
Hannes <harn.hannes@gmail.com>
2024-12-26 20:48:47 +0100

    Configured vim-fugitive status keybind to behave like a toggle

--- nvim/.config/nvim/lua/plugins/utility/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/utility/vim-fugitive.lua
@@ -2,6 +2,26 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
     config = function()
-        vim.api.nvim_set_keymap("n", "<Leader>G", ":Git<CR>", { silent = true, desc = "[G]it status" })
+        vim.keymap.set("n", "<Leader>G", function()
+            local presentwindow
+
+            -- HACK: Check if fugitive git status is open
+            for i = 1, vim.fn.winnr("$") do
+                if vim.fn.getwinvar(i, "&filetype") == "fugitive" then
+                    presentwindow = i
+                end
+            end
+
+            -- HACK: Toggle the git status
+            if presentwindow then
+                vim.cmd(presentwindow .. "wincmd c")
+            else
+                vim.cmd("Git")
+                vim.cmd("resize 10")
+            end
+        end, {
+            silent = true,
+            desc = "[G]it Status",
+        })
     end,
 }
